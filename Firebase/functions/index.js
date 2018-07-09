91d34848112683fff7747f1bc2e1b51969fb6d95
Tomas Smits <tomas@tomassmits.com>
2018-07-09 20:50:24 +0200

    Clean up console log spam and old code

--- Firebase/functions/index.js
+++ Firebase/functions/index.js
@@ -3,17 +3,18 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
+/*
 // Send a message to the device corresponding to the provided
 // registration token.
 exports.testSend = functions.https.onRequest((request, response) => {
     // This registration token comes from the client FCM SDKs.
-    var registrationToken = 'fwlJSrHgzXc:APA91bEUjDkpBQCxvuhN7PL1dY0yVEUYl1RIzNRsygDgu0TPcwEKlo_YM75o838ZhUG0FsvzA11zLZWyp33JPm9HJ0MwJfn67EoVM747tDUfpevZZ_OwFUZuMD4r7GaWo63gftb2Il3l0Ti28S6cUDDl9rryJMWE1g';
+    var registrationToken = 'fNO3qZtpUvM:APA91bHs29uQonpIe5T4siLN0k-8SO6jCfLPEzeLiYNMkud7RO2MfVY8sN_-ZJBQSNK6kMfJ10g6Dl_WieAK54I5AveyUiaHhLcSRHdp0Ob2-HQVM_V8K7s0jTL4BUHnuZ8Fy5cpIOz2NGhE6IAickBI2Dz_hAsgaQ';
 
     console.log("Sending notification");
 
     const payload = {
       notification: {
-        title: 'There is someone at the door.',
+        title: '[TEST] There is someone at the door.',
         body: 'Tap here to see who it is!'
       }
     };
@@ -23,19 +24,29 @@ exports.testSend = functions.https.onRequest((request, response) => {
     response.send("OK");
 
 });
+*/
 
-exports.notifOnNewEvent = functions.database.ref('/events/{pushId}')
+exports.notifOnNewEvent = functions.database.ref('/events/{bellId}/{pushId}')
     .onCreate((snapshot, context) => {
-        var registrationToken = 'fwlJSrHgzXc:APA91bEUjDkpBQCxvuhN7PL1dY0yVEUYl1RIzNRsygDgu0TPcwEKlo_YM75o838ZhUG0FsvzA11zLZWyp33JPm9HJ0MwJfn67EoVM747tDUfpevZZ_OwFUZuMD4r7GaWo63gftb2Il3l0Ti28S6cUDDl9rryJMWE1g';
-
-        console.log("Sending notification " + context);
+        var bellId = context.params.bellId;
+        console.log("Sending notification for bell " + bellId);
 
         const payload = {
           notification: {
-            title: 'There is someone at the door.',
+            title: 'There is someone at the door [' + bellId + '].',
             body: 'Tap here to see who it is!'
           }
         };
 
-        return admin.messaging().sendToDevice(registrationToken, payload);
+        var promiseList = [];
+
+        return snapshot.ref.parent.parent.parent.child('conf').child(bellId).child('subscriptions').once('value').then(
+            function (snap) {
+                snap.forEach(function(child) {
+                    var childKey = child.key;
+                    var childVal = child.val();
+                    promiseList.push( admin.messaging().sendToDevice(childKey, payload) );
+                });
+                return Promise.all(promiseList);
+            });
     });
